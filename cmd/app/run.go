876c992442b86a52ce50c72c8ce8279976f55a74
dontubaby <dontubaby@yandex.ru>
2025-02-22 07:47:00 +0900

    v1

--- cmd/app/run.go
+++ cmd/app/run.go
@@ -1,27 +1,27 @@
-package main
+package app
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"parser_server/internal/config"
+	"parser_server/server"
 
 	"google.golang.org/grpc"
-	pb "parser_server/server/pb"
+	"parser_server/server/pb"
 )
 
-type Server struct {
-	pb.UnimplementedParseServiceServer
-}
-
-func Run(cfg Config) error {
+func Run(cfg config.Config) error {
 	//устанавливаем соединение
 	lis, err := net.Listen("tcp", cfg.ServerAddress)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		return fmt.Errorf("failed to listen: %v", err)
 	}
 
 	// Создаем gRPC сервер
 	s := grpc.NewServer()
-	pb.RegisterParseServiceServer(s, &Server{})
+	// Регистрируем сервис парсинга в gRPC
+	pb.RegisterParseServiceServer(s, server.NewServer(cfg))
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
